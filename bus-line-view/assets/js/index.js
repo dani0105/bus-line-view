a541de6159005d16fd96a7a8549e20411bf928d4
CACU CRC <carloscu@hotmail.es>
2021-11-15 20:49:49 -0600

    try fix select

--- bus-line-view/assets/js/index.js
+++ bus-line-view/assets/js/index.js
@@ -13,10 +13,21 @@ let target = null;
 
 let paradasLayer;
 
-var estilo_selected = new ol.style.Style({
+var estilo_source = new ol.style.Style({
     image: new ol.style.Circle({
         radius: 7.5,
         fill: new ol.style.Fill({ color: 'red' }),
+        stroke: new ol.style.Stroke({
+            color: 'white',
+            width: 2.5
+        })
+    })
+});
+
+var estilo_target = new ol.style.Style({
+    image: new ol.style.Circle({
+        radius: 7.5,
+        fill: new ol.style.Fill({ color: 'yellow' }),
         stroke: new ol.style.Stroke({
             color: 'white',
             width: 2.5
@@ -43,33 +54,41 @@ var estilo_calles = new ol.style.Style({
 });
 
 function onLoad() {
+    var mousePositionControl = new ol.control.MousePosition({
+        coordinateFormat: ol.coordinate.createStringXY(8),
+        projection: 'EPSG:4326',
+    });
+
     map = new ol.Map({
+        controls: ol.control.defaults().extend([mousePositionControl]),
         target: 'map',
         layers: [new ol.layer.Tile({ source: new ol.source.OSM() })],
         view: new ol.View({
             projection: 'EPSG:4326',
-            center: [-84.41, 10.37],
-            zoom: 8
+            center: [-84.41, 9.60],
+            zoom: 8.25
         })
     });
     loadFeatures()
     map.on('click', function(e) {
-        const ft = paradasLayer.getFeaturesAtCoordinate(e.pixel);
-        console.log(ft);
+        var coordsP = map.getCoordinateFromPixel(e.pixel);
+        const ft = map.getClosestFeatureToCoordinate(e.pixel);
+        var coordsF = ft.getGeometry().getCoordinates();
+        console.log("Pixel ", coordsP, "Feature ", coordsF);
         if (!ft) {
             return;
         }
         if (source === null) {
             source = ft;
-            ft.setStyle(estilo_selected);
+            ft.setStyle(estilo_source);
         } else if (target === null) {
             target = ft;
-            ft.setStyle(estilo_selected);
+            ft.setStyle(estilo_target);
         } else {
-            if (source = ft) {
+            if (source === ft) {
                 source = null;
                 ft.setStyle(estilo_paradas);
-            } else if (target = ft) {
+            } else if (target === ft) {
                 target = null;
                 ft.setStyle(estilo_paradas);
             }
